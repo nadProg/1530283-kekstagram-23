22fff7de76c299b8efbf50c6c702f9df33204f0a
Eugene Skrobov <skrobov.ev@gmail.com>
2021-07-05 19:58:10 +0700

    Рефакторит точку входа

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-import { getData } from './api.js';
-import { showAlert } from './alert.js';
-import { initUploadForm}  from './img-upload-form/upload-form.js';
 import { renderPictures } from './picture-gallery/pictures.js';
 import { initBigPicture, setRenderedPictures } from './picture-gallery/big-picture.js';
 import { initFilter } from './picture-gallery/filter.js';
+import { showAlert } from './alert.js';
+import { getData } from './api.js';
+import { initUploadForm}  from './img-upload-form/upload-form.js';
 
 const renderGallery = (pictures) => {
   renderPictures(pictures);
